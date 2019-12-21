94286e0f70932cf53ae0c954d87317d6a3229223
Daniel Marques <up201503822@fe.up.pt>
2019-12-21 16:35:38 +0000

    Fixes issues of the sheet

--- protege/json_to_csv.py
+++ protege/json_to_csv.py
@@ -1,10 +1,13 @@
 import jsonlines
 import csv
 import re
+import collections
 
-i = 1
+
+game_keys = ['id', 'title', 'genres', 'developer', 'publisher', 'release_date', 'features', 'tags', 'description', 'price', 'sentiment', 'percent_positive', 'n_reviews', 'early_access', 'about', 'reviews', 'discount_price', 'franchise']
+game_literals = ['id', 'title', 'release_date', 'description', 'price', 'sentiment', 'percent_positive', 'n_reviews', 'early_access', 'about', 'discount_price', 'franchise']
 games = []
-with jsonlines.open('../steam-scraper/output/dataset.jl') as reader:
+with jsonlines.open('./dataset.jl') as reader:
     for game in reader:
         games.append(game)
 
@@ -18,9 +21,13 @@ files["developer"] = csv.writer(open('./csv/developer.csv', "w+"))
 files["publisher"] = csv.writer(open('./csv/pusblisher.csv', "w+"))
 
 for game in games:
-    for key in list(game.keys()):
+    for key in game_keys:
+        # Ensure same number of columns
+        if not key in game.keys() and key in game_literals:
+            game[key] = None
+
         # If it is a list, it should go to its separate csv with the game id
-        if isinstance(game[key], list):
+        if key in game and isinstance(game[key], list):
             for value in game[key]:
                 # If it is a dict (reviews), write its values (assumes they're literals)
                 if isinstance(value, dict):
@@ -28,5 +35,8 @@ for game in games:
                 else:
                     files[key].writerow([game["id"], value])
             del game[key]
+
+    # Ensure same number of columns
+    game = collections.OrderedDict(sorted(game.items()))
     # Write the remainder values to the games' csv
     files["games"].writerow(game.values())
